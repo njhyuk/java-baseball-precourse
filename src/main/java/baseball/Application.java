4a6e5b26861843df430c04dadf33c29eb890d201
Junhyuk Noh <junhyuk.noh@kurlycorp.com>
2021-10-05 19:49:25 +0900

    게임의 뼈대를 구성 하라

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        Integer[] inputs = {7, 1, 3};
+        ArrayList<Integer> randoms = new ArrayList<Integer>(Arrays.asList(7, 1, 3));
+
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < inputs.length; i++) {
+            boolean isStrike = inputs[i].equals(randoms.get(i));
+            boolean isBall = !isStrike && randoms.contains(inputs[i]);
+
+            strike += isStrike ? 1 : 0;
+            ball += isBall ? 1 : 0;
+        }
+
+        System.out.printf("\n Strike %d", strike);
+        System.out.printf("\n Ball %d", ball);
     }
 }

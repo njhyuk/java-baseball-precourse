21af4941b08a1dc6a5d444922ee2b117d503a9e7
njhyuk <njh2239@gmail.com>
2021-10-05 20:29:58 +0900

    사용자의 인풋을 받도록 변경 하라

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,31 @@
 package baseball;
 
+import nextstep.utils.Console;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        Integer[] inputs = {7, 1, 3};
+
+        System.out.print("숫자를입력해주세요:");
+        String readLine = Console.readLine();
+        ArrayList<Integer> inputs = new ArrayList<Integer>();
+
+        for (String s : readLine.split("")) {
+            inputs.add(Integer.parseInt(s));
+        }
+
         ArrayList<Integer> randoms = new ArrayList<Integer>(Arrays.asList(7, 1, 3));
 
         int strike = 0;
         int ball = 0;
 
-        for (int i = 0; i < inputs.length; i++) {
-            boolean isStrike = inputs[i].equals(randoms.get(i));
-            boolean isBall = !isStrike && randoms.contains(inputs[i]);
+        for (int i = 0; i < inputs.size(); i++) {
+            Integer input = inputs.get(i);
+            
+            boolean isStrike = input.equals(randoms.get(i));
+            boolean isBall = !isStrike && randoms.contains(input);
 
             strike += isStrike ? 1 : 0;
             ball += isBall ? 1 : 0;

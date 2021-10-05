51362a8203bb29512d7a30aba82794573bc70ced
njhyuk <njh2239@gmail.com>
2021-10-05 20:45:06 +0900

    막코딩으로 게임을 완성하라

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,37 +1,58 @@
 package baseball;
 
 import nextstep.utils.Console;
+import nextstep.utils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-
-        System.out.print("숫자를입력해주세요:");
-        String readLine = Console.readLine();
-        ArrayList<Integer> inputs = new ArrayList<Integer>();
-
-        for (String s : readLine.split("")) {
-            inputs.add(Integer.parseInt(s));
+        ArrayList<Integer> randoms = new ArrayList<Integer>();
+        for (int i = 0; i < 3; i++) {
+            randoms.add(Randoms.pickNumberInRange(1, 9));
         }
 
-        ArrayList<Integer> randoms = new ArrayList<Integer>(Arrays.asList(7, 1, 3));
+        System.out.print(randoms);
 
-        int strike = 0;
-        int ball = 0;
+        while (true) {
+            System.out.print("\n 숫자를입력해주세요:");
+            String readLine = Console.readLine();
+            ArrayList<Integer> inputs = new ArrayList<Integer>();
 
-        for (int i = 0; i < inputs.size(); i++) {
-            Integer input = inputs.get(i);
-            
-            boolean isStrike = input.equals(randoms.get(i));
-            boolean isBall = !isStrike && randoms.contains(input);
+            for (String s : readLine.split("")) {
+                inputs.add(Integer.parseInt(s));
+            }
 
-            strike += isStrike ? 1 : 0;
-            ball += isBall ? 1 : 0;
+            int strike = 0;
+            int ball = 0;
+
+            for (int i = 0; i < inputs.size(); i++) {
+                Integer input = inputs.get(i);
+
+                boolean isStrike = input.equals(randoms.get(i));
+                boolean isBall = !isStrike && randoms.contains(input);
+
+                strike += isStrike ? 1 : 0;
+                ball += isBall ? 1 : 0;
+            }
+
+            if (strike > 0) {
+                System.out.printf("\n %d 스트라이크", strike);
+            }
+
+            if (ball > 0) {
+                System.out.printf("\n %d 볼", ball);
+            }
+
+            if (strike == 0 && ball == 0) {
+                System.out.print("낫싱");
+            }
+
+            if (strike == 3) {
+                System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+                // todo 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
+                break;
+            }
         }
-
-        System.out.printf("\n Strike %d", strike);
-        System.out.printf("\n Ball %d", ball);
     }
 }
